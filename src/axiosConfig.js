9e52bc022f67b834398dabba158312c01fd6bef8
sumeya1234 <sumsum8336@gmail.com>
2024-10-25 00:50:32 -0700

    Update on axios config

--- src/axiosConfig.js
+++ src/axiosConfig.js
@@ -4,7 +4,7 @@ const token = localStorage.getItem('token');
 
 const axiosBase = axios.create({
   // baseURL: 'http://localhost:5500/api', // using  local host 
-  baseURL:'https://evangadi-forum-deploy-1-sc9r.onrender.com',  // using Render
+  baseURL:'https://evan-forum-deploy.onrender.com',  // using Render
   headers: {
     Authorization: token ? `Bearer ${token}` : '',
   },
